0e21688a8d0b391d96cd29e82e22b181ff62929f
Moises <mhdezcoronado@gmail.com>
2022-03-05 09:36:36 +0100

    Create deals model controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :deals
   devise_for :users
 
   root 'splash#index'
---
+++ test/factories/deals.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :deal do
+    name { "MyString" }
+    amount { "9.99" }
+    user { nil }
+  end
+end
